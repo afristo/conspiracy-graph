b81b10b504bfc015fb8875858e7a944a864dc4b8
André Fristo <52044302+afristo@users.noreply.github.com>
2024-12-01 06:48:06 -0600

    Improved NER and word count filtering for model inputs

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -1,3 +1,6 @@
+# Import third party libraries
+import spacy
+
 # Import native libraries
 import logging.handlers
 import json
@@ -8,11 +11,37 @@ log = logging.getLogger("logger")
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler())
 
+# Load spaCy"s pre-trained NER model
+nlp = spacy.load("en_core_web_sm")
+
+
+def filter_entries(text):
+    """
+    Takes a string of text and determines if it is worth feeding into the model. If so, it is returned.
+
+    :param text: The text that is being evaluated.
+    """
+
+    # If the text has less than 10 words, drop the text
+    if len(text.split()) < 10:
+
+        return None
+    
+    # Apply named entity recognition to the text
+    doc = nlp(text)
+
+    # If there are no entities, drop the text
+    if len(doc.ents) == 0:
+
+        return None
+    
+    # If the tests are passed, return the original text
+    return text
+
 
 def clean_comments(extracted_data_filepath):
     """
     Reads a JSONL file line by line, applies conditional logic, and writes matching lines to a new file.
-    Optimized version to minimize file I/O and improve efficiency.
 
     :param extracted_data_filepath: Path to the input JSONL file.
     """
@@ -20,7 +49,7 @@ def clean_comments(extracted_data_filepath):
     # Split the path into components
     directory, filename = os.path.split(extracted_data_filepath)
 
-    # Replace the directory from 'raw_data' to 'prepped_data'
+    # Replace the directory from "raw_data" to "prepped_data"
     new_directory = directory.replace("extracted_data", "prepped_data")
 
     # Rename the new file by appending "_prepped.jsonl" to the end
@@ -30,7 +59,7 @@ def clean_comments(extracted_data_filepath):
     jsonl_output_file = os.path.join(new_directory, new_filename)
 
     # Open the input and output files
-    with open(extracted_data_filepath, 'r') as infile, open(jsonl_output_file, 'w') as outfile:
+    with open(extracted_data_filepath, "r") as infile, open(jsonl_output_file, "w") as outfile:
 
         # Use a list to accumulate valid lines for batch writing
         valid_lines = []
@@ -43,29 +72,24 @@ def clean_comments(extracted_data_filepath):
                 # Parse the line as JSON
                 obj = json.loads(line.strip())
 
+                # Try to extract title and body
+                author = obj.get("author")
+                body = obj.get("body")
+
                 # Remove lines where the AutoModerator is commenting
-                if obj["author"] == "AutoModerator":
+                if author == "AutoModerator":
 
                     log.debug(f"Skipping line due to AutoModerator comment: {obj}")
 
                     continue
 
-                # Remove lines where the comment has been removed or deleted
-                if obj["body"] == "[deleted]" or obj["body"] == "[removed]":
+                # Pass the body through the filter
+                body = filter_entries(body)
 
-                    log.debug(f"Skipping line due to comment being deleted: {obj}")
+                # If body passed the filter, add it
+                if body is not None:
 
-                    continue
-
-                # Remove lines where the comment is incredibly short
-                if len(obj["body"].split()) < 3:
-
-                    log.debug(f"Skipping line due to length: {obj}")
-
-                    continue
-
-                # If no errors, accumulate the valid line
-                valid_lines.append(json.dumps(obj))
+                    valid_lines.append(json.dumps({"text": body}))
 
             except json.JSONDecodeError:
                 log.info(f"Skipping invalid JSON line: {line.strip()}")
@@ -89,7 +113,6 @@ def clean_comments(extracted_data_filepath):
 def clean_submissions(extracted_data_filepath):
     """
     Reads a JSONL file line by line, applies conditional logic, and writes matching lines to a new file.
-    Optimized version to minimize file I/O and improve efficiency.
 
     :param extracted_data_filepath: Path to the input JSONL file.
     """
@@ -97,7 +120,7 @@ def clean_submissions(extracted_data_filepath):
     # Split the path into components
     directory, filename = os.path.split(extracted_data_filepath)
 
-    # Replace the directory from 'raw_data' to 'prepped_data'
+    # Replace the directory from "raw_data" to "prepped_data"
     new_directory = directory.replace("extracted_data", "prepped_data")
 
     # Rename the new file by appending "_prepped.jsonl" to the end
@@ -107,22 +130,10 @@ def clean_submissions(extracted_data_filepath):
     jsonl_output_file = os.path.join(new_directory, new_filename)
 
     # Open the input and output files
-    with open(extracted_data_filepath, 'r') as infile, open(jsonl_output_file, 'w') as outfile:
+    with open(extracted_data_filepath, "r") as infile, open(jsonl_output_file, "w") as outfile:
 
         # Use a list to accumulate valid lines for batch writing
         valid_lines = []
-
-        # Use strings to identify deleted or removed post components
-        deleted_or_removed = [
-            "[deleted]",
-            "deleted",
-            "[deleted",
-            "deleted]",
-            "[removed]",
-            "removed",
-            "[removed",
-            "removed]"
-            ]
 
         # Enumerate through each line in the input file
         for line in infile:
@@ -132,22 +143,23 @@ def clean_submissions(extracted_data_filepath):
                 # Parse the line as JSON
                 obj = json.loads(line.strip())
 
-                # Remove lines where the title and body are both incredibly short
-                if len(obj["title"].split()) < 3 and len(obj["body"].split()) < 3:
+                # Try to extract title and body
+                title = obj.get("title")
+                body = obj.get("body")
 
-                    log.debug(f"Skipping line due to length: {obj}")
+                # Pass the title and body through the filter
+                title = filter_entries(title)
+                body = filter_entries(body)
 
-                    continue
+                # If title passed the filter, add it
+                if title is not None:
 
-                # If the body was deleted or removed, set it None
-                if obj["body"] in deleted_or_removed:
+                    valid_lines.append(json.dumps({"text": title}))
 
-                    log.debug(f"Skipping line due to length: {obj}")
+                # If body passed the filter, add it
+                if body is not None:
 
-                    obj["body"] = None
-
-                # If no errors, accumulate the valid line
-                valid_lines.append(json.dumps(obj))
+                    valid_lines.append(json.dumps({"text": body}))
 
             except json.JSONDecodeError:
                 log.info(f"Skipping invalid JSON line: {line.strip()}")
@@ -175,8 +187,11 @@ if __name__ == "__main__":
     the transformer model.
     """
 
+    # Load spaCy"s pre-trained named entity recognition model
+    nlp = spacy.load("en_core_web_sm")
+
     # Open the config JSON file
-    with open("./scripts/prep_data_config.json", 'r') as file:
+    with open("./scripts/prep_data_config.json", "r") as file:
 
         # Read the data into a dictionary
         config = json.load(file)
@@ -187,15 +202,16 @@ if __name__ == "__main__":
         log.info(f"Processing {key} data...")
 
         # Extract the filepath and the values we want to extract from the raw files
-        extracted_data_filepath = value["path"]
+        extracted_data_filepath = value.get("path")
+        type = value.get("type")
 
         # If we have submissions data, call the function specific for cleaning up the submissions data
-        if key in ("Conspiracy Theories Submissions", "Conspiracy Submissions"):
+        if type == "submissions":
 
             clean_submissions(extracted_data_filepath)
 
         # If we have comment data, call the function specific for cleaning up the comment data
-        elif key in ("Conspiracy Theories Comments", "Conspiracy Comments"):
+        elif type == "comments":
 
             clean_comments(extracted_data_filepath)
 
